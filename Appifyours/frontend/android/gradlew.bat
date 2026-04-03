0919f6d05cab6aafde829b3988d2fecd5e143a15
JBBSoftech <jbbsoftech@gmail.com>
2026-04-03 12:22:30 +0530

    Update Appifyours/frontend/android/gradlew.bat

--- Appifyours/frontend/android/gradlew.bat
+++ Appifyours/frontend/android/gradlew.bat
@@ -2,7 +2,31 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 @rem
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -10,7 +34,31 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 @rem you may not use this file except in compliance with the License.
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -18,7 +66,31 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 @rem
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -26,7 +98,31 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 @rem
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -34,7 +130,31 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 @rem distributed under the License is distributed on an "AS IS" BASIS,
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -42,7 +162,31 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 @rem See the License for the specific language governing permissions and
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -54,7 +198,43 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @if "%DEBUG%"=="" @echo off
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -66,7 +246,43 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 set DIRNAME=%~dp0
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -74,7 +290,31 @@ if "%DIRNAME%"=="" set DIRNAME=.
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 set APP_BASE_NAME=%~n0
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -86,7 +326,43 @@ set APP_HOME=%DIRNAME%
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -98,7 +374,43 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -110,7 +422,43 @@ set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @rem Find java.exe
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -122,11 +470,59 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 set JAVA_EXE=java.exe
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 %JAVA_EXE% -version >NUL 2>&1
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -138,7 +534,43 @@ if %ERRORLEVEL% equ 0 goto execute
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 echo.
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -146,7 +578,31 @@ echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PA
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 echo.
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -154,7 +610,43 @@ echo Please set the JAVA_HOME variable in your environment to match the
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 location of your Java installation.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -170,7 +662,43 @@ goto fail
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 :findJavaFromJavaHome
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -178,7 +706,43 @@ set JAVA_HOME=%JAVA_HOME:"=%
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -194,7 +758,43 @@ if exist "%JAVA_EXE%" goto execute
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 echo.
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -202,7 +802,31 @@ echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 echo.
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -210,7 +834,43 @@ echo Please set the JAVA_HOME variable in your environment to match the
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 location of your Java installation.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -226,11 +886,71 @@ goto fail
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 :execute
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 @rem Setup the command line
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -246,7 +966,43 @@ set CLASSPATH=%APP_HOME%\\gradle\\wrapper\\gradle-wrapper.jar
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @rem Execute Gradle
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -258,11 +1014,59 @@ set CLASSPATH=%APP_HOME%\\gradle\\wrapper\\gradle-wrapper.jar
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 :end
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 @rem End local scope for the variables with windows NT shell
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -274,7 +1078,43 @@ if %ERRORLEVEL% equ 0 goto mainEnd
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 :fail
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -282,7 +1122,31 @@ rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instea
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 # the _cmd.exe /c_ return code!
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -290,11 +1154,47 @@ set EXIT_CODE=%ERRORLEVEL%
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -306,7 +1206,43 @@ exit /b %EXIT_CODE%
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 :mainEnd
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -318,7 +1254,43 @@ if "%OS%"=="Windows_NT" endlocal
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 :omega
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
